540d728b8dd9292f9c65c11ac078591608a6314b
Dennismwangi <denniskaranjaonline@gmail.com>
2018-06-06 21:52:06 +0300

    working n youtube.js file

--- js/youtube.js
+++ js/youtube.js
@@ -1,22 +1,37 @@
 
-$(document).ready(function () {
+/*$(document).ready(function () {
     $("form").submit(function (event) {
         event.preventDefault();
         $("div#player").empty();
 
         var artiste = $("input#artiste").val();
-        var title = $("input#title").val();
+        var title = $("input#title").val(); */
 
-        var url = `https://api.lyrics.ovh/v1/${artiste}/${title}`;
+            var request = gapi.client.youtube.search.list({
+                part: "snippet",
+                type: "video",
+                q: encodeURIComponent($("#search").val()).replace(/%20/g, "+"),
+                maxResults: 3,
+                order: "viewCount",
+               /* publishedAfter: "2015-01-01T00:00:00Z"*/
+            });
+
+                function init() {
+            gapi.client.setApiKey("AIzaSyBPzdhCEaWtQh16b6H9IAkAmgry2tr3Nsw");
+            gapi.client.load("youtube", "v3", function () {
+                // yt api is ready
+            });
+
+        /*var url = `https://api.lyrics.ovh/v1/${artiste}/${title}`;*/
 
         $.getJSON(url)
             .done(function (data) {
-                /*data.forEach(display =>{ */
-                $("p#display").append(`<p>${data["lyrics"]}</p>`)
+               /* data.forEach(display =>{ */
+                $("#player").append(`<p>${data["video"]}</p>`)
 
-                /* } );*/
+                } );
 
-                console.log(data["lyrics"]);
+               /* console.log(data["lyrics"]);
 
 
 
@@ -24,4 +39,16 @@ $(document).ready(function () {
 
     });
 
-});
+}); */
+
+
+function init() {
+    gapi.client.setApiKey("AIzaSyBPzdhCEaWtQh16b6H9IAkAmgry2tr3Nsw");
+    gapi.client.load("youtube", "v3", function () {
+        // yt api is ready
+    });
+
+} 
+ }
+
+  
